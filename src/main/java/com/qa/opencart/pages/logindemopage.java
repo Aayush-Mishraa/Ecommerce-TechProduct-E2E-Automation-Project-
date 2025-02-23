69ebcb2325412cc2280f68d302f3417278781514
Aayush_MishraaAayush-Mishraa <aayushmishra026@gmail.com>
2025-02-23 19:28:39 +0530

    I added the recuired changes

--- src/main/java/com/qa/opencart/pages/logindemopage.java
+++ src/main/java/com/qa/opencart/pages/logindemopage.java
@@ -3,4 +3,8 @@ package com.qa.opencart.pages;
 public class logindemopage {
 	int as = 22;
 
+	private void priviate() {
+	// TODO Auto-generated method stub
+	 String myCOde = "My code is ready";
+}
 }
